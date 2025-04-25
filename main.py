a5ffa37bb21a33d311881ac5b5bfd8c5ac971b27
yusufjaafar <j.yusufpro@gmail.com>
2025-04-25 10:52:48 +1000

    decided to focus on adding rows and columns to tables.

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-class Row:
-    def __init__(self, row_id, values, table=None, default_value=None):
+class _Row:
+    def __init__(self, row_id, data, table=None, default_value=None):
         self.row_id = row_id
-        self.values = values
+        self.data = data
         self.table = table
-        self.column_ids = table.column_ids # add capability to have no row on its own.
-        self.as_dict = dict(zip(self.column_ids, self.values))
+        self.column_ids = table.column_ids # add capability to have a row on its own.
+        self._as_dict = dict(zip(self.column_ids, self.data))
         self.default_value = default_value
 
     # _update_value is needed so the Column class can update
     # this class without this class trying to update the Column
     # class when it gets updated
     def _update_value(self, column_id, new_value):
-        self.as_dict[column_id] = new_value
-        self.values = list(self.as_dict.values())
+        self[column_id] = new_value
+        self.data = list(self._as_dict.values())
 
     def __getitem__(self, key):
         if key in self.column_ids:
-            return self.as_dict[key]
+            return self._as_dict[key]
         else:
             raise KeyError(f"The key '{key}' does not exist")
 
@@ -28,27 +28,44 @@ class Row:
             raise KeyError(f"The key '{key}' does not exist")
 
     def __iter__(self):
-        return iter(self.values)
+        return iter(self.data)
 
     def __repr__(self):
-        return f"Row({self.values})"
+        return f"Row({self.data})"
 
-class Column:
-    def __init__(self, column_id, values, table, default_value=None):
+    def _merge_column_ids(self, other):
+        merged_column_ids = self.column_ids
+        for column_id in other.column_ids:
+            if column_id not in self.column_ids:
+                merged_column_ids.append(column_id)
+        return merged_column_ids
+
+    def _merge_data(self, other, merged_column_ids):
+        new_row = [self[column_id] if column_id in self.column_ids else self.default_value\
+                        for column_id in merged_column_ids]
+        other_data = other.data
+        # continue here after capability of adding colums is created
+
+    def __add__(self, other):
+        if isinstance(other, _Row):
+            pass
+
+class _Column:
+    def __init__(self, column_id, data, table, default_value=None):
         self.column_id = column_id
-        self.values = values
+        self.data = data
         self.table = table
         self.row_ids = table.row_ids
-        self.as_dict = dict(zip(self.row_ids, self.values))
+        self._as_dict = dict(zip(self.row_ids, self.data))
         self.default_value = default_value
 
     def _update_value(self, row_id, new_value):
-        self.as_dict[row_id] = new_value
-        self.values = list(self.as_dict.values())
+        self[row_id] = new_value
+        self.data = list(self._as_dict.values())
 
     def __getitem__(self, key):
         if key in self.row_ids:
-            return self.as_dict[key]
+            return self._as_dict[key]
         else:
             raise KeyError(f"The key '{key}' does not exist")
 
@@ -60,33 +77,48 @@ class Column:
             raise KeyError(f"The key '{key}' does not exist")
 
     def __iter__(self):
-        return iter(self.values)
+        return iter(self.data)
 
     def __repr__(self):
-        return f"Column({self.values})"
+        return f"Column({self.data})"
 
+    def _merge_row_ids(self, other):
+        merged_row_ids = self.row_ids
+        for row_id in other.row_ids:
+            if row_id not in self.row_ids:
+                merged_row_ids.append(row_id)
+        return merged_row_ids
+
+    def _merge_data(self, other, merged_row_ids):
+        data = list()
+        for row_id in merged_row_ids:
+            data.append([
+                self[row_id] if row_id in self.row_ids else self.default_value,
+                other[row_id] if row_id in other.row_ids else other.default_value
+            ])
+        return data
+
+    # add capability in Table to reposition columns
     def __add__(self, other):
-        if isinstance(other, Column):
+        if isinstance(other, _Column):
             column_ids = [self.column_id, other.column_id]
-            row_ids = self.row_ids
-            for row_id in other.row_ids:
-                if row_id not in self.row_ids:
-                    row_ids.append(row_id)
-            data = list()
-            for row_id in row_ids:
-                data.append([
-                    self[row_id] if row_id in self.row_ids else self.default_value,
-                    other[row_id] if row_id in other.row_ids else other.default_value
-                ])
-            return Table(column_ids, row_ids, data)
+            merged_row_ids = self._merge_row_ids(other)
+            data = self._merge_data(other, merged_row_ids)
         elif isinstance(other, Table):
-            
+            column_ids = [self.column_id] + other.column_ids
+            merged_row_ids = self._merge_row_ids(other)
+            data = self._merge_data(other, merged_row_ids)
+        else:
+            raise TypeError(f"can not add types '_Column' and {type(other).__name__}")
+        return Table(column_ids, merged_row_ids, data)
 
+    def __radd__(self, other):
+        return other + self
 
 
 
 class Table:
-    def __init__(self, column_ids: list, row_ids: list, data):
+    def __init__(self, column_ids: list, row_ids: list, data, default_value=None):
         if len(row_ids) != len(data):
             raise ValueError("row ids do not have the same length as data")
         elif len(column_ids) != len(data[0]):  # assume data has values in it
@@ -94,14 +126,15 @@ class Table:
         self.column_ids = column_ids
         self.row_ids = row_ids
         self.data = data
+        self.default_value = default_value
 
         self.columns = dict()
         for i, col_id in enumerate(column_ids):
             values = [row[i] for row in data]
-            self.columns[col_id] = Column(col_id, values, self)
+            self.columns[col_id] = _Column(col_id, values, self)
         self.rows = dict()
         for row_number, row_id in enumerate(row_ids):
-            self.rows[row_id] = Row(row_id, data[row_number], self)
+            self.rows[row_id] = _Row(row_id, data[row_number], self)
 
     def __getitem__(self, key):
         if key in self.column_ids:
@@ -113,11 +146,69 @@ class Table:
 
     def __setitem__(self, key, value):
         if key in self.column_ids:
-            self.columns[key] = Column(key, value, self)
+            self.columns[key] = _Column(key, value, self)
         elif key in self.row_ids:
-            self.rows[key] = Row(key, value, self)
+            self.rows[key] = _Row(key, value, self)
         else:
             raise KeyError(f"The key '{key}' does not exist")
+
+    def _split_row_ids(self, other):
+        other_only_row_ids = list()
+        matching_row_ids = list()
+        for row_id in other.row_ids:
+            if row_id in self.row_ids:
+                matching_row_ids.append(row_id)
+            else:
+                other_only_row_ids.append(row_id)
+        return other_only_row_ids, matching_row_ids
+
+    def _has_conflicting_data(self, other, matching_column_ids, matching_row_ids):
+        if matching_column_ids == [] or matching_row_ids == []:
+            return False, None, None
+        else:
+            for row_id, column_id in zip(matching_row_ids, matching_column_ids):
+                if self[row_id][column_id] != other[row_id][column_id]:
+                    return True, row_id, column_id
+            return False, None, None
+
+    def _split_column_ids(self, other):
+        other_only_column_ids = list()
+        matching_column_ids = list()
+        for column_id in other.column_ids:
+            if column_id in self.column_ids:
+                matching_column_ids.append(column_id)
+            else:
+                other_only_column_ids.append(column_id)
+        return other_only_column_ids, matching_column_ids
+
+
+    def _merge_data(self, other):
+        other_only_column_ids, matching_column_ids = self._split_column_ids(other)
+        other_only_row_ids, matching_row_ids = self._split_row_ids(other)
+        has_conflicts, row_id, column_id = self._has_conflicting_data(other, matching_column_ids, matching_row_ids)
+        if has_conflicts:
+            raise ValueError(f"can not add tables with conflicting values at {row_id} and {column_id}")
+
+        merged_data = self.data
+        for row_id, column_id in zip(matching_row_ids, matching_column_ids):
+            pass
+
+
+
+
+
+
+    def __add__(self, other):
+        if not (isinstance(other, Table) or isinstance(other, _Row) or isinstance(other, _Column)):
+            raise TypeError(f"can not add types 'Table' and {type(other).__name__}")
+        if self.default_value != other.default_value:
+            raise ValueError("can not merge tables with mismatched default values")
+        if isinstance(other, _Row):
+            if other.row_id in self.row_ids:
+
+
+
+
 
 
 
